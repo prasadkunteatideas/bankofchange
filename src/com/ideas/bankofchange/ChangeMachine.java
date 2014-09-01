dc3689b9d05989d316471859f933fdc206db317c
prasadkunte <prasadkunte@gmail.com>
2014-09-01 16:58:48 +0530

    nth checkin

--- src/com/ideas/bankofchange/ChangeMachine.java
+++ src/com/ideas/bankofchange/ChangeMachine.java
@@ -8,20 +8,28 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class ChangeMachine {
-	private TreeMap<Integer, Integer> denominationMap=new TreeMap<Integer, Integer>(Collections.reverseOrder());
-	private static final List<Integer> validDenominations= Arrays.asList(1000, 100, 50, 20, 10, 5, 2, 1);
+	// testing
+	private TreeMap<Integer, Integer> denominationMap = new TreeMap<Integer, Integer>(
+			Collections.reverseOrder());
+	private static final List<Integer> validDenominations = Arrays.asList(1000,
+			100, 50, 20, 10, 5, 2, 1);
 
 	public Map<Integer, Integer> getCurrentDenominationsSituation() {
 		return denominationMap;
 	}
 
-	public boolean loadValidDenominations(final Map<Integer, Integer> myDenominationMap) {
-		boolean validDenominationsLoaded=true;
+	public boolean loadValidDenominations(
+			final Map<Integer, Integer> myDenominationMap) {
+		boolean validDenominationsLoaded = true;
 		for (Integer denomination : myDenominationMap.keySet()) {
-			if(isDenominationValid(denomination))
-				denominationMap.put(denomination, myDenominationMap.get(denomination)+(denominationMap.get(denomination)!=null?denominationMap.get(denomination):0));
-			else{
-				validDenominationsLoaded=false;
+			if (isDenominationValid(denomination))
+				denominationMap
+						.put(denomination,
+								myDenominationMap.get(denomination)
+										+ (denominationMap.get(denomination) != null ? denominationMap
+												.get(denomination) : 0));
+			else {
+				validDenominationsLoaded = false;
 				break;
 			}
 		}
@@ -29,66 +37,77 @@ public class ChangeMachine {
 	}
 
 	public Map<Integer, Integer> getChange(final Integer myNote) {
-		if(!isDenominationValid(myNote)){
+		if (!isDenominationValid(myNote)) {
 			return returnIncomingNote(myNote);
 		}
-		TreeMap<Integer, Integer> returnedChange=new TreeMap<Integer, Integer>();
-		Map<Integer, Integer> clonedDenominationMap=cloneOriginalDenominationState();
-		Integer incomingAmount=myNote;
-		
+		TreeMap<Integer, Integer> returnedChange = new TreeMap<Integer, Integer>();
+		Map<Integer, Integer> clonedDenominationMap = cloneOriginalDenominationState();
+		Integer incomingAmount = myNote;
+
 		for (Integer denomination : denominationMap.keySet()) {
-			Integer availableNotesForThisDenomination = denominationMap.get(denomination);
-			if(canChangeBeGivenFromThisDenomination(myNote, denomination, availableNotesForThisDenomination)){
-				int numberOfNotesToReturn = numberOfNotesToReturnForThisDenomination(incomingAmount, denomination,availableNotesForThisDenomination);
+			Integer availableNotesForThisDenomination = denominationMap
+					.get(denomination);
+			if (canChangeBeGivenFromThisDenomination(myNote, denomination,
+					availableNotesForThisDenomination)) {
+				int numberOfNotesToReturn = numberOfNotesToReturnForThisDenomination(
+						incomingAmount, denomination,
+						availableNotesForThisDenomination);
 				returnedChange.put(denomination, numberOfNotesToReturn);
-				incomingAmount=incomingAmount-(denomination*numberOfNotesToReturn);
-				denominationMap.put(denomination, availableNotesForThisDenomination-numberOfNotesToReturn);
+				incomingAmount = incomingAmount
+						- (denomination * numberOfNotesToReturn);
+				denominationMap.put(denomination,
+						availableNotesForThisDenomination
+								- numberOfNotesToReturn);
 			}
-			if(incomingAmount==0)
+			if (incomingAmount == 0)
 				break;
 		}
-		if(incomingAmount!=0){
-			denominationMap=(TreeMap<Integer, Integer>) clonedDenominationMap;
+		if (incomingAmount != 0) {
+			denominationMap = (TreeMap<Integer, Integer>) clonedDenominationMap;
 			return returnIncomingNote(myNote);
 		}
 		return returnedChange;
 	}
-	
+
 	public List<Integer> getFinishedDenominations() {
-		List<Integer> finishedDenominations=new ArrayList<Integer>();
+		List<Integer> finishedDenominations = new ArrayList<Integer>();
 		for (Integer denomination : denominationMap.descendingKeySet()) {
-			if(denominationMap.get(denomination)==0)
+			if (denominationMap.get(denomination) == 0)
 				finishedDenominations.add(denomination);
 		}
 		return finishedDenominations;
 	}
-	
+
 	private boolean isDenominationValid(Integer denomination) {
 		return validDenominations.contains(denomination);
 	}
 
 	private TreeMap<Integer, Integer> returnIncomingNote(final Integer myNote) {
-		TreeMap<Integer, Integer> returnedChange=new TreeMap<Integer, Integer>();
+		TreeMap<Integer, Integer> returnedChange = new TreeMap<Integer, Integer>();
 		returnedChange.put(myNote, 1);
 		return returnedChange;
 	}
-	
-	private boolean canChangeBeGivenFromThisDenomination(final Integer myNote, Integer denomination, Integer availableNotesForThisDenomination) {
-		return denomination < myNote && availableNotesForThisDenomination>0;
+
+	private boolean canChangeBeGivenFromThisDenomination(final Integer myNote,
+			Integer denomination, Integer availableNotesForThisDenomination) {
+		return denomination < myNote && availableNotesForThisDenomination > 0;
 	}
 
-	private int numberOfNotesToReturnForThisDenomination(Integer incomingAmount, Integer denomination, Integer availableNotesForThisDenomination) {
-		int numberOfNotesExpectedToReturn=incomingAmount/denomination;
-		int numberOfNotesToReturn=numberOfNotesExpectedToReturn<availableNotesForThisDenomination?numberOfNotesExpectedToReturn:availableNotesForThisDenomination;
+	private int numberOfNotesToReturnForThisDenomination(
+			Integer incomingAmount, Integer denomination,
+			Integer availableNotesForThisDenomination) {
+		int numberOfNotesExpectedToReturn = incomingAmount / denomination;
+		int numberOfNotesToReturn = numberOfNotesExpectedToReturn < availableNotesForThisDenomination ? numberOfNotesExpectedToReturn
+				: availableNotesForThisDenomination;
 		return numberOfNotesToReturn;
 	}
-	
+
 	private Map<Integer, Integer> cloneOriginalDenominationState() {
-		Map<Integer, Integer> clonedMap=new TreeMap<Integer, Integer>();
+		Map<Integer, Integer> clonedMap = new TreeMap<Integer, Integer>();
 		for (Integer denomination : denominationMap.descendingKeySet()) {
 			clonedMap.put(denomination, denominationMap.get(denomination));
 		}
 		return clonedMap;
 	}
-	
+
 }
